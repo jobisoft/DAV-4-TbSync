ff24bd990b5f89e7cac549a83f771e769c553f0b
John Bieling <john.bieling@gmx.de>
2018-08-23 20:38:11 +0200

    also catch uriencoded hrefs when doing href match

--- content/tools.js
+++ content/tools.js
@@ -355,9 +355,10 @@ dav.tools = {
 
     getNodeTextContentFromMultiResponse: function (response, path, href = null, status = "200") {
         for (let i=0; i < response.multi.length; i++) {
-            let node = dav.tools.evaluateNode(response.multi[i].node, path);
-            if (node !== null && (href === null || response.multi[i].href == href) && response.multi[i].status == status) 
+            let node = dav.tools.evaluateNode(response.multi[i].node, path);            
+            if (node !== null && (href === null || response.multi[i].href == href || decodeURIComponent(response.multi[i].href) == href || response.multi[i].href == decodeURIComponent(href)) && response.multi[i].status == status) {
                 return node.textContent;
+            }
         }
         return null;
     },
