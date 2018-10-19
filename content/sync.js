55134426a71b91d226b6ee330e1b7a60df056e43
John Bieling <john.bieling@gmx.de>
2018-10-19 21:28:07 +0200

    Revert "add debug"
    This reverts commit 08d30bd4dc11456518413e70283325e1404d68c7.

--- content/sync.js
+++ content/sync.js
@@ -487,8 +487,6 @@ dav.sync = {
 
         for (let i=0; i < cards2catch.length; i+=maxitems) {
             let request = dav.tools.getMultiGetRequest(cards2catch.slice(i, i+maxitems));
-            let a=0;
-            let m=0;
             if (request) {
                 tbSync.setSyncState("send.request.remotechanges", syncdata.account, syncdata.folderID);
                 let cards = yield dav.tools.sendRequest(request, syncdata.folderID, "REPORT", syncdata, {"Depth": "1", "Content-Type": "application/xml; charset=utf-8"});
@@ -504,18 +502,15 @@ dav.sync = {
                         switch (vCardsChangedOnServer[id]) {
                             case "ADD":
                                 dav.tools.addContact (addressBook, id, data, etag, syncdata);
-                                a++;
                                 break;
 
                             case "MOD":
                                 dav.tools.modifyContact (addressBook, id, data, etag, syncdata);
-                                d++;
                                 break;
                         }
                     }
                 }
             }
-            tbSync.dump("Multiget","added: "+a+" modified:" +m);
         }
     }),
 
