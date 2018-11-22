8c5e8d93aaf05ff6885fa41f178bf3a965267f7d
John Bieling <john.bieling@gmx.de>
2018-11-22 21:44:58 +0100

    icloud provider not needed

--- bootstrap.js
+++ bootstrap.js
@@ -21,7 +21,6 @@ let onInitDoneObserver = {
         //load all providers of this provider Add-on into TbSync (one at a time, obey order)
        try {
             yield tbSync.loadProvider(thisID, "dav", "//dav4tbsync/content/dav.js");
-            yield tbSync.loadProvider(thisID, "icloud", "//dav4tbsync/content/icloud.js");
         } catch (e) {}
     })
 }
