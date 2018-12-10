a0f6ea20e5b70f022f032cf0f21ddd091e5e4f7c
John Bieling <john.bieling@gmx.de>
2018-12-10 13:27:39 +0100

    remove invalid chars from XML

--- content/tools.js
+++ content/tools.js
@@ -33,6 +33,37 @@ dav.tools = {
         return null;
     },
     
+    /**
+     * Removes XML-invalid characters from a string.
+     * @param {string} string - a string potentially containing XML-invalid characters, such as non-UTF8 characters, STX, EOX and so on.
+     * @param {boolean} removeDiscouragedChars - a string potentially containing XML-invalid characters, such as non-UTF8 characters, STX, EOX and so on.
+     * @return : a sanitized string without all the XML-invalid characters.
+     *
+     * Source: https://www.ryadel.com/en/javascript-remove-xml-invalid-chars-characters-string-utf8-unicode-regex/
+     */
+    removeXMLInvalidChars: function (string, removeDiscouragedChars = true)
+    {
+        // remove everything forbidden by XML 1.0 specifications, plus the unicode replacement character U+FFFD
+        var regex = /((?:[\0-\x08\x0B\f\x0E-\x1F\uFFFD\uFFFE\uFFFF]|[\uD800-\uDBFF](?![\uDC00-\uDFFF])|(?:[^\uD800-\uDBFF]|^)[\uDC00-\uDFFF]))/g;
+        string = string.replace(regex, "");
+     
+        if (removeDiscouragedChars) {
+            // remove everything not suggested by XML 1.0 specifications
+            regex = new RegExp(
+                "([\\x7F-\\x84]|[\\x86-\\x9F]|[\\uFDD0-\\uFDEF]|(?:\\uD83F[\\uDFFE\\uDFFF])|(?:\\uD87F[\\uDF"+
+                "FE\\uDFFF])|(?:\\uD8BF[\\uDFFE\\uDFFF])|(?:\\uD8FF[\\uDFFE\\uDFFF])|(?:\\uD93F[\\uDFFE\\uD"+
+                "FFF])|(?:\\uD97F[\\uDFFE\\uDFFF])|(?:\\uD9BF[\\uDFFE\\uDFFF])|(?:\\uD9FF[\\uDFFE\\uDFFF])"+
+                "|(?:\\uDA3F[\\uDFFE\\uDFFF])|(?:\\uDA7F[\\uDFFE\\uDFFF])|(?:\\uDABF[\\uDFFE\\uDFFF])|(?:\\"+
+                "uDAFF[\\uDFFE\\uDFFF])|(?:\\uDB3F[\\uDFFE\\uDFFF])|(?:\\uDB7F[\\uDFFE\\uDFFF])|(?:\\uDBBF"+
+                "[\\uDFFE\\uDFFF])|(?:\\uDBFF[\\uDFFE\\uDFFF])(?:[\\0-\\t\\x0B\\f\\x0E-\\u2027\\u202A-\\uD7FF\\"+
+                "uE000-\\uFFFF]|[\\uD800-\\uDBFF][\\uDC00-\\uDFFF]|[\\uD800-\\uDBFF](?![\\uDC00-\\uDFFF])|"+
+                "(?:[^\\uD800-\\uDBFF]|^)[\\uDC00-\\uDFFF]))", "g");
+            string = string.replace(regex, "");
+        }
+     
+        return string;
+    },
+
     xmlns: function (ns) {
         let _xmlns = [];
         for (let i=0; i < ns.length; i++) {
@@ -457,7 +488,7 @@ dav.tools = {
         let xml = null;
         let oParser = (Services.vc.compare(Services.appinfo.platformVersion, "61.*") >= 0) ? new DOMParser() : Components.classes["@mozilla.org/xmlextras/domparser;1"].createInstance(Components.interfaces.nsIDOMParser);
         try {
-            xml = oParser.parseFromString(text, "application/xml");
+            xml = oParser.parseFromString(dav.tools.removeXMLInvalidChars(text), "application/xml");
         } catch (e) {
             //however, domparser does not throw an error, it returns an error document
             //https://developer.mozilla.org/de/docs/Web/API/DOMParser
