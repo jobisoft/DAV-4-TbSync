c16f7536a1a5b850604b46369336c9c4f80ce5bb
John Bieling <john.bieling@gmx.de>
2019-03-27 21:17:50 +0100

    alphabetical order for posteo

--- content/dav.js
+++ content/dav.js
@@ -27,11 +27,11 @@ var dav = {
 
     serviceproviders: {
         "fruux" : {icon: "fruux", caldav: "https://dav.fruux.com", carddav: "https://dav.fruux.com"},
-        "posteo" : {icon: "posteo", caldav: "https://posteo.de:8443", carddav: "posteo.de:8843"},
         "icloud" : {icon: "icloud", caldav: "https://caldav.icloud.com", carddav: "https://contacts.icloud.com"},
         "yahoo" : {icon: "yahoo", caldav: "https://caldav.calendar.yahoo.com", carddav: "https://carddav.address.yahoo.com"},
         "gmx.net" : {icon: "gmx", caldav: "https://caldav.gmx.net", carddav: "https://carddav.gmx.net/.well-known/carddav"},
         "gmx.com" : {icon: "gmx", caldav: "https://caldav.gmx.com", carddav: "https://carddav.gmx.com/.well-known/carddav"},
+        "posteo" : {icon: "posteo", caldav: "https://posteo.de:8443", carddav: "posteo.de:8843"},
     },
     
     //https://bugzilla.mozilla.org/show_bug.cgi?id=669675
