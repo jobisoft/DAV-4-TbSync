1e8fbeb0f20440ae61d9c8ad324473a47511c9ea
John Bieling <john.bieling@gmx.de>
2019-08-21 20:31:49 +0200

    getProviderName()

--- content/provider.js
+++ content/provider.js
@@ -64,9 +64,9 @@ var Base = class {
 
 
     /**
-     * Returns nice string for the name of provider for the add account menu.
+     * Returns string for the name of provider for the add account menu.
      */
-    static getNiceProviderName() {
+    static getProviderName() {
         return tbSync.getString("menu.name", "dav");
     }
 
