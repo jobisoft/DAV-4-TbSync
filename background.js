610a3c049343f97a16f4f23f0c7a980e2ad2561d
John Bieling <john.bieling@gmx.de>
2020-07-14 11:12:57 +0200

    do not mess with updates

--- background.js
+++ background.js
@@ -4,7 +4,7 @@ function handleUpdateAvailable(details) {
 
 async function main() {
   // just by registering this listener, updates will not install until next restart
-  messenger.runtime.onUpdateAvailable.addListener(handleUpdateAvailable);
+  //messenger.runtime.onUpdateAvailable.addListener(handleUpdateAvailable);
 
   await messenger.LegacyBootstrap.registerChromeUrl([ ["content", "dav4tbsync", "content/"] ]);
   await messenger.LegacyBootstrap.registerBootstrapScript("chrome://dav4tbsync/content/bootstrap.js");  
